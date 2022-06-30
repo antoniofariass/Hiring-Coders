7e19368ecedb7f8133a096c748738e6a008d6dd5
AntonioFarias <antoniotercerio@gmail.com>
2022-06-29 23:28:04 -0300

    back to studies

--- Fase 1/Week 2/01-Basic_JS.js
+++ Fase 1/Week 2/01-Basic_JS.js
@@ -1,1 +1,10 @@
-var nome 
+var nome = "AF"
+var numeros = 1 
+numeros = 2 
+
+var todos = "valores armazenados"
+
+console.log("a frase armazenada é" + todos)
+
+var entrada = prompt("insira um valor:")
+
